b45b278ad097c1d008518ec1edae485804a208a1
Erenfc <mtherfcker3@gmail.com>
2024-02-26 20:59:21 +0530

    track modified

--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -9,18 +9,17 @@ function Track() {
   const { mode } = context;
 
   return (
-    <div style={{ overflow: 'auto', height: '500px', width: '100%', display: 'flex', justifyContent: 'center' }}>
-      {/* Center the component horizontally */}
+    <div style={{ overflowX: 'auto', overflowY: 'hidden', height: '250px', width: '100%', display: 'flex', justifyContent: 'center' }}>
       <section className="specific-section">
-        <div className="container mx-auto px-5 md:py-5">
+        <div className="container mx-auto p-5 md:p-5">
           <div className="flex text-center">
+
             {/* Grocery Shopping Section */}
-            <div className="p-4" style={{ minWidth: '300px' }}>
+            <div className="p-4" style={{ minWidth: 'clamp(200px, 33%, 300px)' }}>
               <div
                 className="grocery-card border-2 hover:shadow-xl hover:shadow-gray-200 border-gray-200 bg-gray-100 shadow-inset p-4 rounded-lg"
                 style={{ backgroundColor: mode === 'dark' ? 'rgb(46, 49, 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
                 <GiShoppingCart className="text-green-600 w-12 h-12 mb-3 inline-block" />
-
                 <h2 className="title-font font-medium text-lg text-gray-900 mb-2" style={{ color: mode === 'dark' ? 'white' : '' }}>
                   Grocery Shopping
                 </h2>
@@ -29,21 +28,33 @@ function Track() {
             </div>
 
             {/* Fast Delivery Section */}
-            <div className="p-4" style={{ minWidth: '300px' }}>
+            <div className="p-4" style={{ minWidth: 'clamp(200px, 33%, 300px)' }}>
               <div
                 className="grocery-card border-2 hover:shadow-xl hover:shadow-gray-200 border-gray-200 bg-gray-100 shadow-inset p-4 rounded-lg"
                 style={{ backgroundColor: mode === 'dark' ? 'rgb(46, 49, 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
                 <BsTruck className="text-blue-600 w-12 h-12 mb-3 inline-block" />
 
                 <h2 className="title-font font-medium text-lg text-gray-900 mb-2" style={{ color: mode === 'dark' ? 'white' : '' }}>
-                  Fast Delivery
+                  Fast Delivery in 20 mins
                 </h2>
                 <p className="delivery-description leading-relaxed">Get your order delivered in 20-30 minutes.</p>
               </div>
             </div>
 
             {/* Free Shipping Over 100rs Section */}
-            <div className="p-4" style={{ minWidth: '300px' }}>
+            <div className="p-4" style={{ minWidth: 'clamp(200px, 33%, 300px)' }}>
+              <div
+                className="grocery-card border-2 hover:shadow-xl hover:shadow-gray-200 border-gray-200 bg-gray-100 shadow-inset p-4 rounded-lg"
+                style={{ backgroundColor: mode === 'dark' ? 'rgb(46, 49, 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
+                <GiTakeMyMoney className="text-purple-600 w-12 h-12 mb-3 inline-block" />
+
+                <h2 className="title-font font-medium text-lg text-gray-900 mb-2" style={{ color: mode === 'dark' ? 'white' : '' }}>
+                  Free Shipping Over 100rs
+                </h2>
+                <p className="delivery-description leading-relaxed">Enjoy free shipping on orders over 100rs.</p>
+              </div>
+            </div>
+            <div className="p-4" style={{ minWidth: 'clamp(200px, 33%, 300px)' }}>
               <div
                 className="grocery-card border-2 hover:shadow-xl hover:shadow-gray-200 border-gray-200 bg-gray-100 shadow-inset p-4 rounded-lg"
                 style={{ backgroundColor: mode === 'dark' ? 'rgb(46, 49, 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
